5b4fda974051bbb18642def7a903814e6a702078
VincentLass <77613243+VincentLass@users.noreply.github.com>
2022-04-02 20:02:58 +0200

    Add files via upload

---
+++ beans/Personnes.js
@@ -0,0 +1,12 @@
+class Personnes {
+    id
+    nom
+    prenom
+    constructor(nom,prenom){
+        this.nom = nom
+        this.prenom = prenom
+        this.id = null
+    }
+}
+
+module.exports = Personnes;
---
+++ beans/Spe.js
@@ -0,0 +1,10 @@
+class Spe{
+    nom
+    id
+    constructor(nom){
+        this.nom = nom
+        this.id = null
+    }
+}
+
+module.exports = Spe;
